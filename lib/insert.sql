fa91f5bb35aede1c3d9b60824d1833c78d77f2d2
Anthony Lam <al3101@nyu.edu>
2018-05-22 01:27:59 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,14 @@
 INSERT INTO users (id, name, age) VALUES (1, "Anthony", 25), (2, "Ben", 27), (3, "Jack", 15), (4, "Sammy", 24), (5, "Seung", 26), (6, "Marco", 21), (7, "Eric", 45), (8, "Leana", 25), (9, "Sebastian", 24), (10, "Richard", 30), (11, "Raph", 27), (12, "Kevin", 26), (13, "Dawei", 18), (14, "Josh", 20), (15, "Alex", 55), (16, "Raymond", 34), (17, "Moho", 33), (18, "Henry", 29), (19, "Hector", 38), (20, "Lyn", 24); 
 
-INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Fund my MacBook", "technology", 1500, "2018-05-20", "2018-6-20"), (2, "Save the animals", "charity", 10500, "2018-02-20", "2018-04-20"), (3, "Build a soup kitchen", "charity", 5500, "2017-02-20", "2017-02-28"), (4, "Fund my piano", "music", 20000, "2018-03-11", "2018-10-11"), (5, "Plant 100 trees", "environment", 7000, "2018-02-20", "2019-04-20"), (6, "Clothes for homeless", "charity", 5000, "2018-02-20", "2018-05-20"), (7, "Fund startups", "technology", 100000, "2018-02-20", "2019-04-20"), (8, "Build a dog park", "charity", 1500, "2018-02-20", "2018-04-20"), (9, "Super special awesome watch", "technology", 1500, "2018-02-20", "2018-03-20"), (10, "Fund my TV show", "technology", 50000, "2018-02-20", "2019-04-20");
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "Fund my MacBook", "technology", 1500, "2018-05-20", "2018-6-20"), 
+(2, "Save the animals", "charity", 10500, "2018-02-20", "2018-04-20"), 
+(3, "Build a soup kitchen", "charity", 5500, "2017-02-20", "2017-02-28"), 
+(4, "Fund my piano", "music", 20000, "2018-03-11", "2018-10-11"), 
+(5, "Plant 100 trees", "environment", 7000, "2018-02-20", "2019-04-20"), 
+(6, "Clothes for homeless", "charity", 5000, "2018-02-20", "2018-05-20"), 
+(7, "Fund startups", "technology", 100000, "2018-02-20", "2019-04-20"), 
+(8, "Build a dog park", "charity", 1500, "2018-02-20", "2018-04-20"), 
+(9, "Super special awesome watch", "technology", 1500, "2018-02-20", "2018-03-20"), (10, "Fund my TV show", "technology", 50000, "2018-02-20", "2019-04-20");
 
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES (1, 10.00, 1, 2),
 (2, 20.00, 1, 3),
